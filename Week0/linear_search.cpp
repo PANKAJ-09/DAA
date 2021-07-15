de7db7b29828c07f6ec9e28b729383443b67ea98
Pankaj Kumar <87271002+PANKAJ-09@users.noreply.github.com>
2021-07-15 12:49:37 +0530

    Update linear_search.cpp

--- Week0/linear_search.cpp
+++ Week0/linear_search.cpp
@@ -1,1 +1,34 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+        int key;
+        cin>>key;
+        int c=0;
+        bool found=false;
+        for(int i=0;i<n;i++)
+        {
+              c++;
+            if(a[i]==key)
+            {
+                cout<<"element found at index "<<i<<" and total number of comparisions are "<<c<<endl;
+                found=true;
+                break;
+            }
+        }
+        if(!found)
+            cout<<"element not found and total number of comparisions are "<<c<<endl;
+    }
+    return 0;
+}
